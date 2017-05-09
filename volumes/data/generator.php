bc5fae4782b720253a242603e788a0d94ff44cb8
Oleksandr Strilkov <os@root.ua>
2017-05-09 21:24:12 +0300

    Add class UserGenerator

--- volumes/data/generator.php
+++ volumes/data/generator.php
@@ -1,8 +1,32 @@
 <?php
-$array = [
-  'forname' => 'Иван',
-  'lastname' => 'Петров',
-  'age' => '33',
-];
-$string = json_encode($array, JSON_UNESCAPED_UNICODE);
-echo($string);
+
+class UserGenerator {
+
+  private $num;
+
+  public function __construct($num) {
+    $this->num = $num;
+  }
+  public function generate() {
+    $array = [
+      'forname' => 'Иван',
+      'lastname' => 'Петров',
+      'age' => '33',
+    ];
+
+    $string = [ $array ];
+
+    if( $this->num > 1 ) {
+      for ( ; $this->num > 1; --$this->num) {
+        array_push($string, $array);
+      }
+    }
+
+    $ret = json_encode($string, JSON_UNESCAPED_UNICODE);
+    echo($ret);
+  }
+
+};
+
+$ug = new UserGenerator(2);
+$ug->generate();
